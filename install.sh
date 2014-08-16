ac413140ab3e26d7aff813556c417c653d90777b
Albert Freeman <albert.freeman@gmail.com>
2014-08-16 11:20:27 -0500

    more hammering on install to *actually* install files

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 BACKUP_DIR=~/.holdfiles
+SOURCE_DIR=$( cd "$( dirname "$0" )" && pwd )
 if [[ -f ~/.bash_profile || -f ~/.bashrc ]]; then
 	mkdir $BACKUP_DIR
 	mv ~/.bash* $BACKUP_DIR
 	mv ~/.inputrc $BACKUP_DIR
+	rm ~/.bash*
+	rm ~/.inputrc
+	cp $SOURCE_DIR/.bash_profile ~
+	cp $SOURCE_DIR/.bashrc ~
 else
 	echo No bash files found.
 fi
